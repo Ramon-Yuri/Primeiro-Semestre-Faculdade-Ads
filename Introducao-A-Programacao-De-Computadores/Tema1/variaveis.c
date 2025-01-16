679c91bda83f7f11e39c93a144e872fc1eb37b27
Ramon Yuri <ramonyuridev@gmail.com>
2025-01-16 09:35:05 -0300

    Adicionando exercicios

---
+++ Introducao-A-Programacao-De-Computadores/Tema1/variaveis.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 20;
+    float altura = 1.80;
+    double salario = 2000.50;
+    char letra = 'R';
+    char nome[10] = 'Ramon';
+
+    //iniciando e declarando variáveis
+}
